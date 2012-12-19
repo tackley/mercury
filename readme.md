7cba27c615d8b8b5921f2aa3a523076a02c41b39
Graham Tackley <graham@tackley.net>
2012-12-19 16:43:53 +0000

    Now decodes the network front!!!!!!!!!!!!!
    OMFG this is so bloody awesome!!!!!!!!!!!!!!!!!!!
    
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- readme.md
+++ readme.md
@@ -11,3 +11,29 @@ Here's the idea:
   (something like a simple list on the left hand side)
 
 
+Update
+
+Well that was the original idea. Some more thoughts:
+
+- what I really want to know is "where has this item of content been promoted"
+
+- e.g. for url X:
+
+  - 9:45-9:50 Sport front -> Auto position 5
+  - 9:45-10:00 UK Network front -> Main trailblock -> position 7 sublink 2
+  - 10:30-10:35 UK network front -> Special trail -> position 1
+  - etc
+
+- how to do this:
+  - every minute, poll all the fronts and store
+     time + url + page + component + position
+  - then given a url
+     - get all the "promotions"
+     - aggregate the contiguous minute entries for the same component + position
+     - return
+
+
+- detecting simple "position" is easy - just do which url in the list of the component
+
+- sublinks is more tricky - can we identify that it is a sublink -
+    yes: if one of its parents is "sublinks" then it's a sublink
---
+++ test/Main.scala
@@ -0,0 +1,75 @@
+import java.io.File
+import java.net.URL
+import org.jsoup.Jsoup
+import collection.JavaConverters._
+import org.jsoup.nodes.Element
+
+case class Link(href: String, componentName: Option[String] = None, isSublink: Boolean)
+
+object Main extends App {
+
+  //val f = new File("nf.html")
+  //val f = new File("sport.html")
+  val doc = Jsoup.parse(new URL("http://www.guardian.co.uk"), 9999)
+
+  val elems = doc.select("a[href^=http:]").asScala
+  println(elems.size + " links")
+
+  def findDataComponent(e: Element): Option[String] =
+    e.parents().asScala
+      .find(_.hasAttr("data-component"))
+      .map(_.attr("data-component"))
+
+
+  val links = for (link <- elems) yield {
+    val href = link.attr("abs:href").takeWhile('?' !=).takeWhile('#' !=)
+    val comp = findDataComponent(link)
+    val isSublink = link.parents().asScala.exists(_.hasClass("sublinks"))
+
+    Link(href, comp, isSublink)
+  }
+
+  //links.map { l => l.href + "" + l.componentName.getOrElse("?") }.foreach(println)
+  case class Promotion(
+    url: String,
+    component: String,
+    topPosition: Int,
+    sublinkPosition: Option[Int]
+  ) {
+    def pretty = component + " position " + topPosition +
+      sublinkPosition.map(" sublink " + _).getOrElse("") +
+      " => " + url
+  }
+
+  val grouped: Map[Option[String], Seq[Link]] = links.groupBy { _.componentName }
+
+  def positionLinks(links: Seq[Link]): Seq[(String, Int, Option[Int])] = {
+    links.distinct.foldLeft(List[(String, Int, Option[Int])]()) {
+      case (acc, el) if !el.isSublink => acc :+ (el.href, acc.lastOption.map(_._2 + 1).getOrElse(1), None)
+      case (acc, el) if el.isSublink => acc :+ (el.href, acc.last._2, acc.last._3.map(1 +).orElse(Some(1)))
+    }
+  }
+
+  val proms = for {
+    (optionalComponent, links) <- grouped
+    (href, topPos, sublinkPos) <- positionLinks(links)
+    componentName <- optionalComponent
+  } yield {
+    val simpleComp = componentName.split(":").map(_.trim).filterNot(_.isEmpty).last
+    Promotion(href, simpleComp, topPos, sublinkPos)
+  }
+
+  proms.map(_.pretty).toList.sorted.foreach(println)
+
+//  for ((comp,l) <- grouped) {
+//    val simpleComp = comp flatMap { c =>
+//      c.split(":").map(_.trim).filterNot(_.isEmpty).lastOption
+//    }
+//    println(comp + " ==> " + simpleComp)
+//
+//    for (link <- l.distinct) println("\t" + link.isSublink + " " + link.href)
+//  }
+
+
+
+}
