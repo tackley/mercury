a59d05d2312300a8c7dceae380560026c44e005f
Graham Tackley <graham@tackley.net>
2013-01-03 17:12:28 +0000

    Use task queues to retry on scanning
    And prepare for scanning multiple endpoints. Want to see how much this
    costs before I do that though.

--- src/main/scala/mercury/AdminController.scala
+++ src/main/scala/mercury/AdminController.scala
@@ -4,32 +4,32 @@ import unfiltered.request._
 import unfiltered.response._
 import org.slf4j.LoggerFactory
 import java.net.URL
+import com.google.appengine.api.taskqueue.QueueFactory
+import com.google.appengine.api.taskqueue.TaskOptions.Builder._
 
 class AdminController extends unfiltered.filter.Plan {
   private val log = LoggerFactory.getLogger(getClass)
-
-  import QParams._
+  private val queue = QueueFactory.getQueue("scan-queue")
 
   def intent = {
     case GET(Path("/admin/cron/scan") & Params(p)) =>
-
-      val expected = for {
-        optUrl <- lookup("url") is required("url parameter expected")
-      } yield {
-        val url = new URL(optUrl.get)
-
-        log.info("Scanning {}...", url)
-
-        val promotions = PageScanner.findPromotions(url)
-
-        Store.write(url, promotions)
-
-        ResponseString(promotions.toList.map(_.pretty).sorted.mkString("\n"))
+      val scanInitiated = for (p <- Pages.all) yield {
+        queue.add(withUrl("/admin/task/scan").param("url", p.url.toString))
+        p.name
       }
 
-      expected(p) orFail { fails =>
-        BadRequest ~> ResponseString(fails.map(_.error).mkString("Bad Request", "\n", "\n"))
-      }
+      ResponseString("Scan tasks queued: " + scanInitiated.mkString(", "))
+
+    case POST(Path("/admin/task/scan") & Params(p)) =>
+      val url = p("url").headOption.map(new URL(_)).getOrElse(sys.error("url expected"))
+
+      log.info("Scanning {}...", url)
+
+      val promotions = PageScanner.findPromotions(url)
+
+      Store.write(url, promotions)
+
+      ResponseString(promotions.toList.map(_.pretty).sorted.mkString("\n"))
 
   }
 }
