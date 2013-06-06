5ecb7063e2ed85b7d456d0b795c97a897954db52
Graham Tackley <graham@tackley.net>
2013-06-06 07:59:09 +0100

    Create uberjar

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
-import PlayProject._
+
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object ApplicationBuild extends Build {
 
@@ -9,12 +11,28 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.jsoup" % "jsoup" % "1.7.2",
-      "com.amazonaws" % "aws-java-sdk" % "1.4.3"
+      "com.amazonaws" % "aws-java-sdk" % "1.4.3",
+      "org.scala-lang" % "scala-reflect" % "2.10.1"
     )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      scalaVersion := "2.10.1",
-      scalacOptions := List("-feature", "-deprecation")
+    val main = play.Project(appName, appVersion, appDependencies)
+      .settings(assemblySettings: _*)
+      .settings(
+        scalaVersion := "2.10.1",
+        scalacOptions := List("-feature", "-deprecation"),
+        ivyXML :=
+          <dependencies>
+            <exclude org="commons-logging"/>
+          </dependencies>,
+
+        mainClass in assembly := Some("play.core.server.NettyServer"),
+        jarName in assembly := "app.jar",
+
+        mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) => {
+            case "play/core/server/ServerWithStop.class" => MergeStrategy.first
+            case x => old(x)
+          }}
+
     )
 
 }
