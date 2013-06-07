3fab40a4d3c1aad29bae7a930159fdd5326fe0c5
Graham Tackley <graham@tackley.net>
2013-06-07 14:50:01 +0100

    Create artifact

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
 
+import play.Project._
+
 import sbtassembly.Plugin._
 import AssemblyKeys._
 
@@ -8,6 +10,9 @@ object ApplicationBuild extends Build {
 
     val appName         = "nf-archive"
     val appVersion      = "1.0-SNAPSHOT"
+
+    val playArtifactResources = TaskKey[Seq[(File, String)]]("play-artifact-resources", "Files that will be collected by the deployment-artifact task")
+    val playArtifactFile = SettingKey[String]("play-artifact-file", "Filename of the artifact built by deployment-artifact")
 
     val appDependencies = Seq(
       "org.jsoup" % "jsoup" % "1.7.2",
@@ -32,7 +37,35 @@ object ApplicationBuild extends Build {
             case "play/core/server/ServerWithStop.class" => MergeStrategy.first
             case x => old(x)
           }}
+      )
+      .settings(
+        playArtifactResources <<= (assembly, baseDirectory) map {
+          (assembly, baseDirectory) =>
+            Seq(
+              assembly -> "packages/ophan-time-machine/%s".format(assembly.getName),
+              baseDirectory / "conf" / "deploy.json" -> "deploy.json"
+            )
+        },
+        dist <<= buildDeployArtifact
 
-    )
+      )
+
+
+  private def buildDeployArtifact = (streams, target, playArtifactResources) map {
+    (s, target, resources) =>
+      val distFile = target / "artifacts.zip"
+      s.log.info("Disting " + distFile)
+
+      if (distFile.exists()) {
+        distFile.delete()
+      }
+      IO.zip(resources, distFile)
+
+      // Tells TeamCity to publish the artifact => leave this println in here
+      println("##teamcity[publishArtifacts '%s => .']" format distFile)
+
+      s.log.info("Done disting.")
+      distFile
+  }
 
 }
